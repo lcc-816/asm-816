976dc0ae616091133ba7a3babf6f482f852e1ba4
Kelvin Sherlock <ksherlock@gmail.com>
2015-04-03 13:58:49 -0400

    print address mode

--- main.cpp
+++ main.cpp
@@ -167,6 +167,7 @@ void peephole(Line *line) {
 #endif
 
 
+
 void print(Line *line) {
 
 	while (line) {
@@ -174,9 +175,131 @@ void print(Line *line) {
 		if (line->label) { printf("%s:\n", line->label->c_str()); }
 
 		if (line->instruction) {
+			std::string op;
+			AddressMode mode = line->mode;
+
+			switch(mode) {
+				case kUndefinedAddressMode:
+				case implied:
+				case relative:
+				case relative_long:
+				case block:
+				case interrupt:
+				case stack_relative: // < ?
+					break;
+
+				case stack_relative_y:
+					op = "("; // (< ?
+					break;
+
+				case zp:
+				case zp_x:
+				case zp_y:
+				case zp_relative:
+					op = "<";
+					break;
+
+				case absolute:
+				case absolute_x:
+				case absolute_y:
+					op = "|";
+					break;
+
+				case absolute_long:
+				case absolute_long_x:
+					op = ">";
+					break;
+
+				case immediate:
+					op = "#";
+					break;
+
+				case zp_indirect:
+				case zp_indirect_x:
+				case zp_indirect_y:
+					op = "(<";
+					break;
+
+				case zp_indirect_long:
+				case zp_indirect_long_y:
+					op = "[<";
+					break;
+
+				case absolute_indirect:
+				case absolute_indirect_x:
+					op = "(|";
+					break;
+
+				case absolute_indirect_long:
+					op = "[|";  // ??
+					break;
+
+			}
+
+			if (mode != implied) {
+				op.append(line->operands[0]->to_string());
+			}
+			if (mode == block || mode == zp_relative) {
+				op.push_back(',');
+				op.append(line->operands[1]->to_string());
+			}
+
+			switch(mode)
+			{
+				case kUndefinedAddressMode:
+				case implied:
+				case relative:
+				case relative_long:
+				case block:
+				case interrupt:
+				case immediate:
+				case zp:
+				case absolute:
+				case absolute_long:
+				case zp_relative:
+					break;
+
+				case zp_x:
+				case absolute_x:
+				case absolute_long_x:
+					op.append(",x");
+					break;
+				case zp_y:
+				case absolute_y:
+					op.append(",y");
+					break;
+				case zp_indirect:
+				case absolute_indirect:
+					op.append(")");
+					break;
+				case zp_indirect_y:
+					op.append("),y");
+					break;
+				case zp_indirect_x:
+				case absolute_indirect_x:
+					op.append(",x)");
+					break;
+				case stack_relative:
+					op.append(",s");
+					break;
+				case stack_relative_y:
+					op.append(",s),y");
+					break;
+
+				case absolute_indirect_long:
+				case zp_indirect_long:
+					op.append("]");
+					break;
+
+				case zp_indirect_long_y:
+					op.append("],y");
+					break;
+			}
+
+
 			printf("    %s %s\n", 
 				line->instruction.toString(), 
-				AddressModeToString(line->mode)
+				op.c_str()
 			);
 		}
 
